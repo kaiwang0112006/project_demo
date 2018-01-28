df96b443290abe06c3fe481b498e1f5d8aba3d7a
wangkai1 <wangkai1@weshare.com.cn>
2018-01-28 22:01:57 +0800

    readme

--- tools/fenci.py
+++ tools/fenci.py
@@ -6,6 +6,7 @@ from .multi_apply import *
 import os
 import jieba.posseg as pseg
 
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 TAOBAOFILE = os.path.join(BASE_DIR, 'userdicts', 'taobao.txt')
 STOPWORDSFILE = os.path.join(BASE_DIR, 'userdicts', 'stopwords.txt')
