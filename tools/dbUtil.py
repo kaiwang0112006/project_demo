367cbe8a666a832f0d80f9b12bfea71b6f159327
kaiwang <kaiwang0112006@yahoo.com>
2018-02-06 13:57:09 +0800

    db

--- tools/dbUtil.py
+++ tools/dbUtil.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import pymongo
 import traceback
+import pyodbc
+
 class MongoConnect(object):
     def __init__(self, host, port):
         self.host = host 
@@ -22,3 +24,47 @@ class MongoConnect(object):
         
     def close(self):
         self.client.close()
+
+class mysqldbParse(object):
+    def __init__(self, host='', port=3306, user='', passwd='', dbName='', charset='utf8',connect_timeout=31536000):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.passwd = passwd
+        self.dbName = dbName
+        self.charset = charset
+        self.connect_timeout = connect_timeout
+        
+    def conn_mysql(self):
+        self.conn = pymysql.connect(host=self.host,
+                                    port=self.port,
+                                    user=self.user,
+                                    password=self.passwd,
+                                    db=self.dbName,
+                                    charset=self.charset,
+                                    connect_timeout = self.connect_timeout,
+                                    cursorclass=pymysql.cursors.DictCursor)
+        
+    def insert_sql(self,insertSql):
+        with self.conn.cursor() as cursor:
+            cursor.execute(insertSql)
+            self.conn.commit()
+
+    def close(self):
+        self.conn.close()
+        
+def sqlserverParse(object):
+    def __init__(self, host='', user='', passwd='', dbName=''):
+        self.host = host
+        self.user = user
+        self.passwd = passwd
+        self.dbName = dbName
+        
+    def conn_connect(self):
+        self.conn = pyodbc.connect('DRIVER=(ODBC DRIVER 13 for SQL Server);SERVER='+self.host+';DATABASE='+
+                                   self.dbName+';UID='+self.user+';PWD='+self.passwd)
+        
+
+
+    def close(self):
+        self.conn.close()
