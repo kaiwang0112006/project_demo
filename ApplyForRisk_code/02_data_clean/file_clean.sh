928f8ceb58c3654d347bfd3b2709d996e12fec23
kaiwang <kaiwang0112006@yahoo.com>
2018-06-08 11:50:32 +0800

    rm

--- ApplyForRisk_code/02_data_clean/file_clean.sh
+++
@@ -1,5 +0,0 @@
-path = "../data_mid"
-date = $1
-sed -i 's/\"\"/\"/g' $path/data.$date.screen_all.log
-sed -i 's/\"\[\[/\[\[/g' $path/data.$date.screen_all.log
-sed -i '/^\"$/d' $path/data.$date.screen_all.log
